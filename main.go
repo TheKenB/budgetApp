06600c5e5042c2067c365f81a9fb42ef78bba1f7
Ken <37745141+TheKenB@users.noreply.github.com>
2024-01-06 21:41:51 -0500

    M cleanup

--- main.go
+++ main.go
@@ -12,12 +12,15 @@ import (
 func main() {
 	var res resolution = res720()
 	dGrid := gr.NewGrid([2]int{res.x, res.y})
-	rl.InitWindow(int32(res.x), int32(res.y), "budgeting")
-	rl.SetTargetFPS(60)
 	var font rl.Font = rl.LoadFontEx("fonts/Louis George Cafe Bold.ttf", 50, nil)
 	var entries []enJson.Entries
 	var saved bool = false
+
+	rl.InitWindow(int32(res.x), int32(res.y), "budgeting")
+	rl.SetTargetFPS(60)
+
 	enJson.LoadEntries(&entries)
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
@@ -28,20 +31,20 @@ func main() {
 			enJson.LoadEntries(&entries)
 			saved = false
 		}
-		// //		Draw Grid
-		// for i := 0; i < dGrid.Rows; i++ {
-		// 	rl.DrawLine(0, int32(i*dGrid.Height), int32(res.x), int32(i*dGrid.Height), rl.Blue)
-		// }
-		// for j := 0; j < dGrid.Columns; j++ {
-		// 	rl.DrawLine(int32(j*dGrid.Width), 0, int32(j*dGrid.Width), int32(res.y), rl.Blue)
-		// }
+		//		Draw Grid
+		for i := 0; i < dGrid.Rows; i++ {
+			rl.DrawLine(0, int32(i*dGrid.Height), int32(res.x), int32(i*dGrid.Height), rl.Blue)
+		}
+		for j := 0; j < dGrid.Columns; j++ {
+			rl.DrawLine(int32(j*dGrid.Width), 0, int32(j*dGrid.Width), int32(res.y), rl.Blue)
+		}
 
-		if rl.IsKeyPressed(rl.KeyZ) {
+		if rl.IsKeyPressed(rl.KeySemicolon) {
 			res = res1080()
 			rl.SetWindowSize(res.x, res.y)
 			dGrid = gr.NewGrid([2]int{res.x, res.y})
 		}
-		if rl.IsKeyPressed(rl.KeyX) {
+		if rl.IsKeyPressed(rl.KeyComma) {
 			res = res720()
 			rl.SetWindowSize(res.x, res.y)
 			dGrid = gr.NewGrid([2]int{res.x, res.y})
